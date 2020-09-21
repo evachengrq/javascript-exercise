cc3dea0e7b8e2761c1bd77cd7700e9ba4890abdc
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-21 21:11:10 +0800

    [evacheng] feat: completed array filter test

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,9 @@
 function chooseMultiplesOfThree(collection) {
-  // TODO 1: 在这里写实现代码
+  return collection.filter(num => num % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
-  // TODO 2: 在这里写实现代码
+  return collection.filter((item, index) => collection.indexOf(item) === index);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
