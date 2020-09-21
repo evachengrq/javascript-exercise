42ac2d160cb729a0a2c82d919c662cad320da365
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-22 00:17:37 +0800

    [evacheng]feat: completed class paractice 4

--- 4-class/practice_4/person.js
+++ 4-class/practice_4/person.js
@@ -1,1 +1,15 @@
-// TODO 15: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  basicIntroduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+}
